942f0f6306ccfa26b2d0ddd4776051aba761ae39
BenW2k <benworkman16@gmail.com>
2024-04-19 11:37:20 +0100

    + 2nd Revision, more readable, less mem, O(n) solution

--- Top Interview 150/two-pointer-easy-2.py
+++ Top Interview 150/two-pointer-easy-2.py
@@ -1,20 +1,12 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if s != "":
-            isTrue = False
-            s_list = [x for x in s]
-            t_list = [x for x in t]
-            check_list = []
-
-            j= 0
-            for i in range(len(t_list)):
-                    if t_list[i] == s_list[j]:
-                        check_list.append(s_list[j])
-                        if check_list == s_list:
-                            isTrue = True
-                            return isTrue
-                        else:
-                            j += 1
-            return isTrue
-        else:
-            return True
+        i, j = 0, 0 # Creates the Two Pointers
+        while i < len(s) and j < len(t): # Checks if the true conditions have been met and it is still in range of the list
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
+    
+# Notes - 
+# uses i to as a way to check to if s is in t by counting every time a character in s is found in t and comparing the value of i to the length of t (same value means s is in t)
+# uses check at the end to send the boolean back.
