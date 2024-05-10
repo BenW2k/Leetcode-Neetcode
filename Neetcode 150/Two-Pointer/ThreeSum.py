d896864e3a04ac14496efdaecc8d13a8aee91b9b
BenW2k <benworkman16@gmail.com>
2024-05-10 15:27:24 +0100

    +Renamed to make it file contents more apparent

---
+++ Neetcode 150/Two-Pointer/ThreeSum.py
@@ -0,0 +1,13 @@
+
+def threeSum(nums):
+    i = 0
+    j = 1
+    k = len(nums)
+    triplets = []
+
+
+    while i < k:
+        if nums[i] + nums[j] + nums[k] == 0:
+            
+            triplets.append
+
