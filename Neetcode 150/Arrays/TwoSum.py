e76d230ec42d50eced9eb3440611078290d792ca
BenW2k <benworkman16@gmail.com>
2024-05-12 11:59:39 +0100

    +added comments to make reasoning more clear for future reference

--- Neetcode 150/Arrays/TwoSum.py
+++ Neetcode 150/Arrays/TwoSum.py
@@ -1,7 +1,7 @@
 def twoSum(nums, target):
-    map = {}
-    for i, n in enumerate(nums):
-        res = target - n
-        if res in map:
-            return [i, map[res]]
-        map[n] = i
+    map = {} # Initialises empty hashmap
+    for i, n in enumerate(nums): # where i is the index and n is the value at that index.
+        res = target - n # The number we're looking for is the result of the sum target - n
+        if res in map: # if the result of this sum is already in our map
+            return [i, map[res]] # return list object [index of current, index of target-n] because question is looking for indexes not values.
+        map[n] = i # if res isn't in map, add the current iteration values to the map where the key is the value in nums and i is the value.
