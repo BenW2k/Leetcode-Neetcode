4cb3435810a7add7a6ea25aa755d6f9474ee583f
BenW2k <benworkman16@gmail.com>
2024-09-28 04:24:39 +0100

    +comments

--- Top Interview 150/Two-Pointer/Med-167-Two-Sum-II.py
+++ Top Interview 150/Two-Pointer/Med-167-Two-Sum-II.py
@@ -5,12 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        i = 0
-        j = len(numbers) - 1
+        # this question states that it is a one-indexed array meaning it starts at 1
+        i = 0 # first pointer (still 0 because loops still operate at 0 regardless)
+        j = len(numbers) - 1 # right pointer
 
         while i < j:
             if numbers[i] + numbers[j] == target:
-                return [i+1, j+1]
+                return [i+1, j+1] # adding 1 to each element in the return list because its one indexed
             elif numbers[i] + numbers[j] > target:
                 j -= 1
             else:
