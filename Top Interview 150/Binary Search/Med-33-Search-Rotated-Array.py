742549bc700515f3dd72efb48dd61234ae9ef631
BenW2k <benworkman16@gmail.com>
2024-09-30 06:12:13 +0100

    +comments

--- Top Interview 150/Binary Search/Med-33-Search-Rotated-Array.py
+++ Top Interview 150/Binary Search/Med-33-Search-Rotated-Array.py
@@ -20,19 +20,19 @@ class Solution(object):
 
         while l <= r:
             m = (l + r) // 2
-            if target == nums[m]:
+            if target == nums[m]: # if m is the target return it
                 return m
             # left sorted portion
-            if nums[l] <= nums[m]:
-                if target > nums[m] or target < nums[l]:
+            if nums[l] <= nums[m]: # if nums l is less than nums m it means we're in the left sorted portion
+                if target > nums[m] or target < nums[l]: # if the target is greater than m or less than l then it means we need to swap to the right sorted portion
                     l = m + 1
                 else:
-                    r = m - 1
+                    r = m - 1 # else continue with left side
 
             # right sorted portion
             else:
-                if target < nums[m] or target > nums[r]:
-                    r= m - 1
-                else:
+                if target < nums[m] or target > nums[r]: # if m is greater or r is less than it means we're in the wrong sorted portion and need to move to the other
+                    r = m - 1
+                else: # else continue as normal in the right side of the array
                     l = m + 1
-        return -1
+        return -1 # return -1 if we don't see the target in there
