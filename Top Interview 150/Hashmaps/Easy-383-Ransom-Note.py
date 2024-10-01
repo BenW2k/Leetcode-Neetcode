d765c9b6a036e06c8adee8cbc4514feb2e673532
BenW2k <benworkman16@gmail.com>
2024-10-01 15:20:13 +0100

    +comments

--- Top Interview 150/Hashmaps/Easy-383-Ransom-Note.py
+++ Top Interview 150/Hashmaps/Easy-383-Ransom-Note.py
@@ -6,11 +6,17 @@ class Solution(object):
         :rtype: bool
         """
         mag = {}
+        # Firstly adding all letters in the magazine string to mag hashmap
+        # Also adds the frequency that each letter occurs
         for i in range(len(magazine)):
             if magazine[i] not in mag:
                 mag[magazine[i]] = 1
             else:
                 mag[magazine[i]] += 1
+        # Iterates over ransomNote and for each letter it reduces the count of that letter in the hashmap by one
+        # If a letter is present but the count in the hashmap is 0 then it returns False
+        # If a letter isn't present it returns False
+        # Else it returns True
         for i in range(len(ransomNote)):
             if ransomNote[i] not in mag:
                 return False
