00486a3033b7e61f1c18e08089325194086fe545
Ben Workman <benworkman16@gmail.com>
2024-10-05 19:02:05 +0100

    +comments

--- Top Interview 150/Hashmaps/Easy-205-Isomorphic-Strings.py
+++ Top Interview 150/Hashmaps/Easy-205-Isomorphic-Strings.py
@@ -5,6 +5,12 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # This problem is about seeing if the strings in one string can be used as a direct 1:1 mapping to another string
+        # each letters can only pair with one letter
+        # Used a hashmap to store pairs and a separate array to store letters from the second string to ensure that we don't have repeats.
+        # I think this solution is O(n) because the maximum amount of times (assuming we're only dealing with english letters) that we could possibly reach the nested
+        # iteration through the seen list is 26 (the 26 letters in the alphabet) hence not making it O(n^2)
+        
         if len(s) != len(t):
             return False
         
