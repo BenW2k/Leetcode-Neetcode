67453ed02e355cc632e52acbe8514d835ff285dc
Ben Workman <benworkman16@gmail.com>
2024-10-05 18:36:20 +0100

    +comments

---
+++ Top Interview 150/Arrays/Med-189-Rotate-Array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # The O(1) mem solution here requires us to rotate the entire array, then rotate the first k elements then rotate the elements after it
+        k = k % len(nums) # This just means we get the remainder of the division (which will always give us a valid number of rotations)
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+        
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
