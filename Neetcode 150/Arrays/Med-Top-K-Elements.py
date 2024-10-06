b9879db04962cc88c7f1870725786c02c9969973
Ben Workman <benworkman16@gmail.com>
2024-10-06 11:30:44 +0100

    +comments

--- Neetcode 150/Arrays/Med-Top-K-Elements.py
+++ Neetcode 150/Arrays/Med-Top-K-Elements.py
@@ -1,5 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+      # Basically you want to add each number to a hashmap with the amount of times they occur in the array
+      # You want to destructure the key-value pairs from the dict and store an array of numbers that occur the amount of times that they're at in the index
+      # e.g all elements that occur twice are at the freq[2]
+      # then you want to initialise another array and iterate from the end of the frequency array, iterating through its subarrays adding numbers
+      # from largest first until there are k elements in the result array (len(res) == k)
+      # then just return res
         map = {}
         freq = [[] for i in range(len(nums) + 1)]
         for i in range(len(nums)):
