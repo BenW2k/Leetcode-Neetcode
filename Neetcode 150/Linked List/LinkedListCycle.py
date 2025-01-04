3f69e6b5e8701d8bb65609b7e77dd43d31d8d718
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-01-04 08:34:38 +0000

    +comments

--- Neetcode 150/Linked List/LinkedListCycle.py
+++ Neetcode 150/Linked List/LinkedListCycle.py
@@ -4,6 +4,10 @@
 #         self.val = val
 #         self.next = next
 
+
+# Basically you want to use the rabbit and hare technique to determine whether thereis a loop
+# This involves using a fast and slow pointer, if there is a loop the fast pointer will eventually be the same as the slow pointer
+# if the fast or fast.next pointers (because you'll get an attribute error trying to f.next.next something where the next point is None) are None, then there is no cycle
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         f = s = head
