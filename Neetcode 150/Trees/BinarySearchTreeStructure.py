96bfcd1d398bd7aa69ea9d7872bfc72d70aa05ce
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-01-07 10:57:02 +0000

    +initial commit - base code for DFS traversal with recursion

---
+++ Neetcode 150/Trees/BinarySearchTreeStructure.py
@@ -0,0 +1,57 @@
+# Binary Tree
+class TreeNode: # Class for a tree node with basic values for initialisation
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def __str__(self): # Method for printing the node, so print(TreeNode) will just print the val of the specified node
+        return str(self.val)
+    
+
+# Recursive DFS Pre Order Traversal Time: O(n), Space: O(n)
+def pre_order(node):
+    if not node:
+        return
+
+    print(node)
+    pre_order(node.left)
+    pre_order(node.right)
+
+# Recursive DFS In Order Traversal Time: O(n), Space: O(n)
+
+def in_order(node):
+    if not node:
+        return
+
+    in_order(node.left)
+    print(node)
+    in_order(node.right)
+
+# Recursive DFS Post Order Traversal Time: O(n), Space: O(n)
+
+def post_order(node):
+    if not node:
+        return
+
+    post_order(node.left)
+    post_order(node.right)
+    print(node)
+
+# Test Case
+A = TreeNode(1)
+B = TreeNode(2)
+C = TreeNode(3)
+D = TreeNode(4)
+E = TreeNode(5)
+F = TreeNode(10)
+
+A.left = B
+A.right = C
+B.left = D
+B.right = E
+C.left = F
+
+pre_order(A)
+in_order(A)
+post_order(A)
