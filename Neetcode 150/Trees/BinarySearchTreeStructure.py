e4d5d5d2059bfc64cb3aa51c09f21de7efa66988
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-01-07 11:37:53 +0000

    + function for binary search tree val search

--- Neetcode 150/Trees/BinarySearchTreeStructure.py
+++ Neetcode 150/Trees/BinarySearchTreeStructure.py
@@ -56,7 +56,7 @@ C.left = F
 # in_order(A)
 # post_order(A)
 
-#Iterative DFS Pre order traversal Time: O(n), Space: O(n)
+# Iterative DFS Pre order traversal Time: O(n), Space: O(n)
 # Tip: This may look counter-intuitive but you are appending the right node to the stack before the left node to keep the left-most nodes always at the top of the stack
 # while there are still left nodes to iterate through, once the left nodes are exhausted, the right nodes and their children will be traversed.
 
@@ -69,3 +69,33 @@ def pre_order_iterative(node):
         if node.right: stk.append(node.right)
         if node.left: stk.append(node.left)
 pre_order_iterative(A)
+
+
+# BFS Level Order Traversal Time: O(n), Space: O(n)
+
+from collections import deque # import the double ended queue data structure for as queues are used for BFS
+
+def level_order(node):
+    q = deque()
+    q.append(node)
+
+    while q:
+        node = q.popleft()
+        print(node)
+        if node.left(): q.append(node.left)
+        if node.right: q.append(node.right)
+
+# Searching a binary search tree
+
+def bst_search(node,target):
+    if not node:
+        return False
+    if node.val == target:
+        return True
+    if target < node.val: return bst_search(node.left, target)
+    else: return bst_search(node.right, target)
+
+
+
+
+
