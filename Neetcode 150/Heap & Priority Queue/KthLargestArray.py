c87d3f0b7a76bd403116b145f164bde47d0582a6
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-01-08 07:26:03 +0000

    +comments

--- Neetcode 150/Heap & Priority Queue/KthLargestArray.py
+++ Neetcode 150/Heap & Priority Queue/KthLargestArray.py
@@ -1,5 +1,6 @@
 import heapq
 class Solution:
+    # creates a max heap by negating values then heapify, pops k-1 elements, then returns the next pop which will be the kth largest (* by -1 to revert negation)
     def findKthLargest(self, nums: List[int], k: int) -> int:
         for i in range(len(nums)):
             nums[i] = -nums[i]
@@ -11,6 +12,7 @@ class Solution:
         return -heapq.heappop(nums)
 
 class Solution:
+    # creates a min heap of k size then at the end of the loop it returns the first element in the list which should be the kth largest element in the array
     def findKthLargest(self, nums: List[int], k: int) -> int:
         min_heap = []
 
