226e75f57de6cd9f62fa7be6e90b7db9eb4e7e33
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-01-09 08:55:28 +0000

    + comments

--- Neetcode 150/Backtracking/CombinationSum.py
+++ Neetcode 150/Backtracking/CombinationSum.py
@@ -1,27 +1,28 @@
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        res, sol = [], []
+        res, sol = [], [] # initialise our results and solution lists
 
         def backtrack(i):
-            if sum(sol) == target:
+            if sum(sol) == target: # no duplicate lists so if the sum of sol is equal to target and solution isn't already in res list we append a copy of sol
                 if sol not in res:
-                    res.append(sol[:])
+                    res.append(sol[:]) # this copy of sol is important because of we did sol instead then we're copying a reference to the object rather than cloning the list
                 return
-            if i == len(nums):
+            if i == len(nums): # return if we reach end of list
                 return
+            
+            if sum(sol) > target: # no negative numbers so if we go over the target we can just return
+                return
+            
             # we don't take
 
-            if sum(sol) > target:
-                return
             backtrack(i+1)
 
             # we take the same num again
+
             sol.append(nums[i])
             backtrack(i)
             sol.pop()
 
-
-            # we take current
 
             # we take
             sol.append(nums[i])
