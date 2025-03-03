61690e536dc48547665c598a2008294a69ca6e43
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-03-03 12:08:52 +0000

    +deque implementation (in theory has faster operations than standard list implementation)

--- Neetcode 150/Stacks/RemoveStars.py
+++ Neetcode 150/Stacks/RemoveStars.py
@@ -8,3 +8,16 @@ class Solution:
             else:
                 stack.pop()
         return "".join(stack)
+    
+
+from collections import deque
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = deque()
+
+        for i, char in enumerate(s):
+            if char != "*":
+                stack.append(char)
+            else:
+                stack.pop()
+        return "".join(stack)
