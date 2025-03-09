355b94e1c31ef34109d4676dd6e847a929ab14d7
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-03-09 04:21:06 +0000

    +recursive dfs solution

--- Neetcode 150/Trees/InverseBinaryTree.py
+++ Neetcode 150/Trees/InverseBinaryTree.py
@@ -17,3 +17,14 @@ class Solution:
             if node.right: q.append(node.right)
             if node.left: q.append(node.left)
         return root
+ # Recursive solution (DFS) time O(n) space O(n)
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
