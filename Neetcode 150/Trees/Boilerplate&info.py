a68ca660441e852533e7a24414f5735ffe9b9009
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-03-09 02:45:32 +0000

    +initial comments

---
+++ Neetcode 150/Trees/Boilerplate&info.py
@@ -0,0 +1,36 @@
+'''BFS
+
+Commonly used when finding the shortest path 
+exploring all nodes at a given level
+Commonly implemented with Queue datastructure FIFO manner
+
+
+DFS
+
+Commonly used when checking if a path exists
+Exploring all possible paths
+Commonly implemented with a stack with a FILO approach
+
+Preorder traversal - current node, left child, right child
+- Used when making a copy of a tree
+- Getting a prefix expression
+
+Inorder traversal - left child, current node, right child
+- Getting the nodes in a sorted order in a binary search tree (where the left is smaller and right is larger)
+- For getting infix expression 
+
+Postorder traversal - left child, right child, current node
+- Used when deleting a tree
+- Calculating postfix expression
+- Calculating size of directories in file
+
+Easy remember:
+Pre - before children
+In - inbetween children
+Post - after children 
+
+Binary search trees are sorted binary trees such that everything on the left of a node is smaller than it and everything right of a node is greater than it
+Good at organising things and fast lookup times'''
+
+
+
