91d3356bfb0d30e4ad14c4c2c83f1e5d72f4f20e
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-03-18 17:30:36 +0000

    +clarifying comments

--- Top Interview 150/Arrays/Med-274-H-Index.py
+++ Top Interview 150/Arrays/Med-274-H-Index.py
@@ -4,16 +4,19 @@ class Solution:
         # O(n^2) loop through list for each val, note if valid h val, return highest num
         # sorting the array could be useful 
         n = len(citations)
-        paper_counts = [0] * (n+1)
 
-        for c in citations:
-            paper_counts[min(n, c)] += 1
+        # Important no
+        paper_counts = [0] * (n+1) # this is an array that shows the count of all num of citations where the last value is a bucket for any val greater than n
+
+        for c in citations: # the number of paper counts for each paper
+            paper_counts[min(n, c)] += 1 # we take the minimum of n and c here because if c is greater than n we increment the >=n bucket
         
-        h = n
+        h = n # sets h to n for clarity
         papers = paper_counts[n]
+        # papers is used to show the number of papers that have at least h notations
 
-        while papers < h:
-            h -= 1
-            papers += paper_counts[h]
+        while papers < h: # This is basically saying 'while we havent found a valid h value'
+            h -= 1 # we decrement h by 1
+            papers += paper_counts[h] # and add the num of papers at h to the total papers seen variable then the loop continues
         
-        return h
+        return h # return the h value
