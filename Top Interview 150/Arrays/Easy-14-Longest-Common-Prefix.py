7dac01229b37371f32258e843de869d4da9372eb
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-03-19 07:28:25 +0000

    alt worse solution

--- Top Interview 150/Arrays/Easy-14-Longest-Common-Prefix.py
+++ Top Interview 150/Arrays/Easy-14-Longest-Common-Prefix.py
@@ -13,3 +13,15 @@ class Solution(object):
                 return ans
             ans += first[i]
         return ans
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = sorted(strs)
+        res = []
+        for i in range(min(len(strs[0]),len(strs[-1]))):
+            if strs[0][i] != strs[-1][i]:
+                return "".join(res)
+            res.append(strs[0][i])
+        return "".join(res)
+                
