807c763727228a7facf8dec7ba310ae193a6c8e5
Ben Workman <benworkman16@gmail.com>
2025-06-22 14:08:32 +0100

    + comments, time comp O(k) where k is len of input string, mem is O(k) for inserts, O(1) else

--- Top Interview 150/Tries/Med-208-Implement-Trie.py
+++ Top Interview 150/Tries/Med-208-Implement-Trie.py
@@ -1,13 +1,20 @@
+# Create your own node object, which has a hashmap (for easy lookup) and an endOfWord boolean to check if its a word stored in the Trie
+
 class TreeNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
 
 class Trie(object):
+    
+    # Initialise a root node called self.root which we can use to start the operations.
 
     def __init__(self):
         self.root = TreeNode()
+    
 
+    # We set curr to root node then check if the characters are in the children (going down tree), if not, we add them in place and finally set the last character
+    # to the end of the word
     def insert(self, word):
         curr = self.root
 
@@ -17,6 +24,10 @@ class Trie(object):
             curr = curr.children[c]
         curr.endOfWord = True  
 
+    # Similar process, set root node but if c isnt in children we return false and if it makes its way to the end of the string we return the 'end of word' bool
+    # The reason we return this and not a simple True is because you could have a word like applet and searching apple would result in a True statement
+    # However, apple is not in the tree, but applet is, so we return the endOfWord, if apple has been inserted into the tree itself then it will have returned True
+    # else False
     def search(self, word):
         curr = self.root
 
@@ -25,7 +36,8 @@ class Trie(object):
                 return False
             curr = curr.children[c]
         return curr.endOfWord
-        
+    
+    # This is similar to the others, we basically loop through the prefix and if c isn't in children we simply return False
 
     def startsWith(self, prefix):
         curr = self.root
