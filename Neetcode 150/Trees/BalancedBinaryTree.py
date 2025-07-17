699ee80c117e6fa2866cceb4c8a397221aeffb2a
BenW2k <100225853+BenW2k@users.noreply.github.com>
2025-07-17 09:46:21 +0100

    +comments

--- Neetcode 150/Trees/BalancedBinaryTree.py
+++ Neetcode 150/Trees/BalancedBinaryTree.py
@@ -7,19 +7,22 @@
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        # So we use recursive DFS to find height of the sub trees, then we compare absolute values to find the difference between
+        # the subtree heights, if the diff is greater than 1 then we know the tree isn't balanced.
+        # We use -1 to 'bubble out' of the recursive loop because we've already met our condition
 
         def dfs(curr):
-            if not curr:
+            if not curr: #base case
                 return 0
             right = dfs(curr.right)
-            if (right == -1):
+            if (right == -1): #checking if already bubbling up
                 return -1
             left = dfs(curr.left)
             if (left == -1):
                 return -1
-            if abs(left-right) > 1:
-                return -1
-            return 1 + max(left, right)
+            if abs(left-right) > 1: # computes the difference
+                return -1 
+            return 1 + max(left, right) # returns height of the tree
         if dfs(root) == -1:
             return False
         else:
